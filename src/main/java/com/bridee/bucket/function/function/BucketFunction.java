18e6702a4d40038dd50faed89f9d0394765957f1
ian15963 <ian.santos@sptech.school>
2025-01-01 22:37:10 -0300

    feat: Creating post endpoint to trigger the upload file process.

---
+++ src/main/java/com/bridee/bucket/function/function/BucketFunction.java
@@ -0,0 +1,35 @@
+package com.bridee.bucket.function.function;
+
+import com.bridee.bucket.function.dto.FileRequest;
+import com.bridee.bucket.function.service.BucketService;
+import com.microsoft.azure.functions.*;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+@RequiredArgsConstructor
+public class BucketFunction {
+
+    private final BucketService bucketService;
+
+    @FunctionName("uploadFile")
+    public HttpResponseMessage uploadFile(@HttpTrigger(name = "request", methods = HttpMethod.POST, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<FileRequest> request,
+                                          ExecutionContext executionContext){
+
+        if (Objects.isNull(request)){
+            throw new IllegalArgumentException("request cannot be null");
+        }
+        executionContext.getLogger().info("Trying to upload the file with name %s".formatted(request.getBody().getFileName()));
+        bucketService.uploadFile(request.getBody());
+
+        return request.createResponseBuilder(HttpStatus.OK)
+                .header("Content-Type", "application/json")
+                .build();
+    }
+
+}
